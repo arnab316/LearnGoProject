ade9cda8adad0ed322588cdc6967ec25c1617c03
Arnab Golder <arnabgolder7@gmail.com>
2024-01-16 19:55:55 +0530

    base commit

---
+++ index.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var (
+	//	val_1   int    = 10
+	//	message string = "Arnab"
+	//	val_2   int    = 20
+	//)
+	//Variable Declaration with No Type
+	//var isNym = 20
+	// Shorter version of the variable
+	//myname := "Arnab Golder"
+	const x = 10
+
+	fmt.Println(x)
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# This is my golang code
+
+- my first learning on golang
